e237f2fc81586148416fade7073eb109b48c8351
Felipe Corniani de Genaro <felipegenaro@outlook.com.br>
2020-05-19 16:10:17 -0300

    more examples of recursion

--- fibonacci.py
+++ fibonacci.py
@@ -10,21 +10,21 @@ Fibonacci_Sequence(11)
 
 # Fibonacci Usign Recursion
 fp = 0
-def Recursive_Fibonacci(n):
+def Recursive_Fibonacci_Sequence(n):
         global fp
 
         if (n <= 1):
                 fp = 1
                 return 1
 
-        t = Recursive_Fibonacci(n - 1)
+        t = Recursive_Fibonacci_Sequence(n - 1)
         f = t + fp
         fp = t
         print(fp)
         
         return f
 
-print(Recursive_Fibonacci(9))
+print(Recursive_Fibonacci_Sequence(9))
 
 
 # Fibonacci Generator also uses Recursion
@@ -37,3 +37,27 @@ def Fibonacci(fib_nums):
 # loop through the generator
 for item in Fibonacci(11):
         print(item)
+
+
+
+# Simplified examples:
+
+# Iterative Implementation
+def iterative_fibonacci(fnum):
+        result = [0, 1]
+
+        for i in range (2, fnum):
+                a = result[i - 1]
+                b = result[i - 2]
+
+                result.append(a + b)
+        
+        return result[fnum]
+
+
+# Recursive Implementation
+def Recursive_Fibonacci(rnum):
+        if (rnum < 2):
+                return rnum
+
+        return Recursive_Fibonacci(rnum - 1) + Recursive_Fibonacci(rnum - 2)
--- recursion.py
+++ recursion.py
@@ -1,8 +1,11 @@
-def recursive_sum(n):
+# Sum
+
+# Fail Implementation
+def Recursive_Sum(n):
         if (n > 100):
                 return n;
         else:
-                return (n + recursive_sum(n - 1))
+                return (n + Recursive_Sum(n - 1))
 
 
 # you always need a reachable Base Case
@@ -11,13 +14,35 @@ def recursive_sum(n):
 # the Stack is Last In First Out queue and has all the tasks that will be excecuted
 
 
-def recursive_sum(n):
+def Recursive_Sum(n):
         if (n <= 1):
                 return n
         else:
-                return (n + recursive_sum(n - 1))
+                return (n + Recursive_Sum(n - 1))
 
 
 # for example n = 3, here the output that we have is 6
 # that's because the function will be executed 3 times
-# the outputs of each call will be { 1 ; 2 + 1 ; 3 + 3 }
+# the outputs of each call will be { (1) ; (2 + 1) ; (3 + 3) }
+
+
+
+# Factorial
+
+# Iterative Implementation
+def Iterative_Factorial(fact):
+        if (fact == 0 or fact == 1):
+                return 1
+        
+        for i in range (fact-1, 1, -1):
+                fact = fact * i
+
+        return fact
+
+
+# Recursive Implementation
+def Recursive_Factorial(fnum):
+        if (fnum == 0 or fnum == 1):
+                return 1
+
+        return fnum * Recursive_Factorial(fnum - 1)
